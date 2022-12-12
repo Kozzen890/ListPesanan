b10f9cb7d90425fd3003f016abc1677dd236eaed
Kozzen <timotiuswinzen@gmail.com>
2022-12-12 12:34:04 +0700

    second push

---
+++ index.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-light bg-secondary">
+      <div class="container-fluid">
+        <a class="navbar-brand mb-0 h1 ms-auto me-4 text-white">Koki</a>
+      </div>
+    </nav>
+    <div class="container pb-5">
+      <div class="row">
+        <div class="col-md-8 mx-auto text-center">
+          <!-- <h6 class="text-primary">WHY CHOOSE US</h6> -->
+          <img src="assets/img/resto.png" alt="" style="width: 200px ; height: 120px ;" class="mt-4">
+          <h4 class="mt-3">Daftar Pesanan</h4>
+        </div>
+        <table class="table table-bordered mt-5">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">NO</th>
+              <th scope="col">Menu Pesanan</th>
+              <th scope="col">Qty</th>
+              <th scope="col">Keterangan</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row" rowspan="3">1</th>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">NO</th>
+              <th scope="col">Menu Pesanan</th>
+              <th scope="col">Qty</th>
+              <th scope="col">Keterangan</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row" rowspan="3">2</th>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+        <table class="table table-bordered p-5">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">NO</th>
+              <th scope="col">Menu Pesanan</th>
+              <th scope="col">Qty</th>
+              <th scope="col">Keterangan</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row" rowspan="3">3</th>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <footer class="bg-secondary text-white text-center mt-5 pb-2 justify-content-center">
+      <p class="pt-4">Copyright @ 2022 - Manajemen Proyek A11.4503</p>
+    </footer>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
