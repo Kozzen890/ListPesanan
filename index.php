fc95f35bc6538c05884f1562349ef38b45c4837f
Kozzen <timotiuswinzen@gmail.com>
2022-12-13 17:42:25 +0700

    fix title

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 
-    <title>Hello, world!</title>
+    <title>Dinus Resto | List Pesanan</title>
   </head>
   <body>
     <nav class="navbar navbar-light bg-secondary">
@@ -28,7 +28,7 @@
           <img src="assets/img/resto.png" alt="" style="width: 200px ; height: 120px ;" class="mt-4">
           <h4 class="mt-3 mb-5">Daftar Pesanan</h4>
         </div>
-        <div class="col-lg-6 bg-secondary bg-gradient p-5">
+        <div class="col-lg-12 bg-secondary bg-gradient p-5 w-75 align-items-center mx-auto">
           <div class="col-lg-12 bg-white align-center mx-auto w-80 p-2">
             <h4 class="mt-3 ms-2">001</h4>
             <table class="table table-bordered mt-2 mb-4 me-2 ms-auto me-auto" style="width: 80%;">
@@ -57,7 +57,7 @@
           </table>
           </div>
         </div>
-        <div class="col-lg-6 bg-secondary bg-gradient p-5">
+        <div class="col-lg-12 bg-secondary bg-gradient p-5 mx-auto w-75">
           <div class="col-lg-12 bg-white align-center mx-auto w-80 p-2">
             <h4 class="mt-3 ms-2">002</h4>
             <table class="table table-bordered mt-2 mb-4 me-2 ms-auto me-auto" style="width: 80%;">
@@ -86,7 +86,7 @@
           </table>
           </div>
         </div>
-        <div class="col-lg-6 bg-secondary bg-gradient mb-3 p-5">
+        <div class="col-lg-12 bg-secondary bg-gradient mx-auto w-75 mb-3 p-5">
           <div class="col-lg-12 bg-white align-center mx-auto w-80 p-2">
             <h4 class="mt-3 ms-2">003</h4>
             <table class="table table-bordered mt-2 mb-4 me-2 ms-auto me-auto" style="width: 80%;">
@@ -115,35 +115,7 @@
           </table>
           </div>
         </div>
-        <div class="col-lg-6 bg-secondary bg-gradient mb-3 p-5">
-          <div class="col-lg-12 bg-white align-center mx-auto w-80 p-2">
-            <h4 class="mt-3 ms-2">004</h4>
-            <table class="table table-bordered mt-2 mb-4 me-2 ms-auto me-auto" style="width: 80%;">
-            <thead>
-              <tr>
-                <th scope="col" style="width:10%">NO</th>
-                <th scope="col">Menu Pesanan</th>
-                <th scope="col" style="width:10%">Qty</th>
-                <th scope="col">Keterangan</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Nasi Goreng Special</td>
-                <td>2</td>
-                <td>Pedas</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Ice Tea</td>
-                <td>2</td>
-                <td>-</td>
-              </tr>
-            </tbody>
-          </table>
-          </div>
-        </div>
+        
       </div>
     </div>
     <footer class="bg-secondary text-white text-center mt-5 pb-2 justify-content-center">
